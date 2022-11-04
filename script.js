9e85e355f155d451ac7bcc101e8b0541bba6259d
Remy <remy@chaoticmewtral.com>
2022-11-04 12:11:39 -0400

    Add to index

--- script.js
+++ script.js
@@ -2,15 +2,12 @@ const canvas = document.getElementById('canvas');
 const canvasHeight = canvas.offsetHeight;
 const canvasWidth = canvas.offsetWidth;
 
-const square = document.getElementById('square');
+
 
 canvas.addEventListener('click', function getPosition(e) {
     const xCoord = (100 * ((e.clientX - canvas.offsetLeft) / canvasWidth)) + '%';
     const yCoord = (100 * ((e.clientY - canvas.offsetTop) / canvasHeight)) + '%';
 
-    square.style.position = 'relative';
-    square.style.top = yCoord;
-    square.style.left = xCoord;
     console.log(xCoord, yCoord);
 });
 
